f2351d10cabff4038df2c3ff6c6b8c8c62c6573d
yo-is4ac <yo.isaac.romero@gmail.com>
2025-12-26 19:56:50 -0300

    fix/structure: now it's decent at least the approach rs

--- script.sh
+++ script.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-wd=$(pwd)
+WD="$(echo $HOME)/Downloads"
 
-url="https://discord.com/api/download?platform=linux&format=deb"
-wget -O ./discord.deb $url
+if [ -d "$WD" ]; then
+  if dpkg -s "discord" &> /dev/null; then
+    rm -rf $WD/discord*
 
-sudo dpkg -i $wd/discord.deb
-rm -rf $wd/discord.deb
+    INSTALLED_DISCORD_VERSION=$(dpkg -s discord | grep -P "\d{1}.\d{1}.\d{3}" | sed 's/[^0-9]//g')
+    
+    URL="https://discord.com/api/download?platform=linux&format=deb"
+    wget -O $WD/discord.deb $URL
+
+    REMOTE_DISCORD_VERSION=$(dpkg-deb -I $WD/discord*.deb | grep "Version" | grep -P "\d{1}.\d{1}.\d{3}" | sed 's/[^0-9]//g') 
+    
+    if [[ "$INSTALLED_DISCORD_VERSION" != "$REMOTE_DISCORD_VERSION" ]]; then
+      sudo dpkg -i $WD/discord.deb
+    else 
+      echo "Latest version already installed"
+    fi
+
+    rm -rf $WD/discord.deb 
+  fi
+fi
